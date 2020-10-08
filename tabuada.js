00be9a8f7dd7eefc7b253b5382bf147c006fa656
Matheus <playermaster10like@gmail.com>
2020-10-08 20:04:45 -0300

    Alterações

--- tabuada.js
+++ tabuada.js
@@ -1,6 +1,7 @@
 let numero= prompt("Digite um número inteiro positivo");
+let numero2= Number(numero);
 
 for(let i = 0; i <= 10; i=i+1)
 {
-    alert(`${numero} X ${i}= ${numero*i}`);
+    alert(`${numero2} X ${i}= ${numero2*i}`);
 }
